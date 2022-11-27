f1bb4ed3cc73e8e3095fd716e0c36466c6b470a2
Gamer <Gamer@DPORT_GAMING>
2022-11-26 22:14:42 -0700

    prompts players and checks scores

---
+++ BlackJackRules.cs
@@ -0,0 +1,32 @@
+﻿namespace BlackJack
+{
+    internal class BlackJackRules
+    {
+        internal static bool IsPlayerBusted(Player player)
+        {
+            return ScoreHand(player) > 21;
+        }
+
+        internal static int ScoreHand (Player player)
+        {
+            int totalScore = 0;
+
+            foreach(Card card in player.Hand)
+            {
+                totalScore += card.Rank switch
+                {
+                    >= Rank.Ace and <= Rank.Nine => (int)card.Rank,
+                    _  => 10
+                };
+            }
+
+            foreach (Card card in player.Hand.Where(c => c.Rank == Rank.Ace))
+            {
+                if (totalScore <= 11)
+                    totalScore += 10;
+            }
+
+            return totalScore;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         const string ROUND_WINS = "Round wins";
         const string MONEY = "Money";
+        const string HAND_TOTAL = "Hand Total";
         static void Main(string[] args)
         {
             Console.WriteLine("Enter seed or enter to auto-generate one:");
@@ -39,18 +40,74 @@
                 Console.WriteLine("No players! The House loses!");
                 return;
             }
+            allPlayers.Add(new Player() {Name = "Dealer"});
 
             foreach (var player in allPlayers)
             {
                 player.Stats[ROUND_WINS] = 0;
                 player.Stats[MONEY] = 0;
+                player.Stats[HAND_TOTAL] = 0;
             }
 
             var activePlayers = new List<Player>(allPlayers);
-
+            
             List<Card> deck = CardTools.BuildADeck();
 
             List<Card> drawPile = CardTools.Shuffle(deck, rng);
+            List<Card> discardPile = new List<Card>();
+
+            foreach (var player in allPlayers) 
+            {
+                player.DrawPile = drawPile;
+                player.DiscardPile = discardPile;
+            }
+
+            var isGameOver = false;
+            while (!isGameOver) 
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    foreach (Player player in allPlayers)
+                    {
+                        player.DrawACard();
+                        Console.WriteLine($"{player.Name} drew {player.Hand.Last()}");
+                    }
+                }
+                //Prompt each player to indicate if they want to STAY or HIT
+                foreach(Player player in allPlayers)
+                {
+                    bool isLoopComplete = false;
+                    while(!isLoopComplete)
+                    { 
+                        Console.WriteLine($"{player.Name}, your hand is worth {BlackJackRules.ScoreHand(player)}");
+                        Console.WriteLine($"{player.Name}, do you (S)TAY or (H)IT?");
+                        var answer = Console.ReadLine() ?? "";
+                        switch (answer.ToUpper())
+                        {
+                            case "S":
+                            case "STAY":
+                                isLoopComplete = true;
+                                break;
+                            case "H":
+                            case "HIT":
+                                player.DrawACard();
+                                Console.WriteLine($"{player.Name} drew {player.Hand.Last()}. " +
+                                    $"Total score is {BlackJackRules.ScoreHand(player)}");
+                                if (BlackJackRules.IsPlayerBusted(player))
+                                {
+                                    Console.WriteLine($"{player.Name} BUSTED!");
+                                }
+                                isLoopComplete = true;
+                                break;
+                            default:
+                                Console.WriteLine("Please enter S to Stay, or H to Hit");
+                                //return to top of foreach loop, but not for the nextg player
+                                break;
+                        }
+                    }
+                }   
+
+            }
         }
     }
 }
